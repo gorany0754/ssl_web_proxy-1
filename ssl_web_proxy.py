1c8466760ec2c3ae2dd4d2b6bf455454b143ef5d
raspberry315 <raspberry315@naver.com>
2017-12-06 06:39:57 +0900

    Update ssl_web_proxy.py

--- ssl_web_proxy.py
+++ ssl_web_proxy.py
@@ -1,5 +1,4 @@
-
-m SocketServer import BaseRequestHandler, ThreadingTCPServer
+from SocketServer import BaseRequestHandler, ThreadingTCPServer
 import os
 import ssl
 import socket
@@ -7,45 +6,6 @@ import socket
 basedir = os.path.abspath(os.path.join(os.path.dirname(__file__), "../"))
 basedir += '\\ssl_web_proxy\\cert-master'
 
-
-def getInfo(data):
-    header = data.split(' ')
-    host = header[1].split(':')[0]
-    if host[:4] == 'www.':
-        name = host[4:]
-    else:
-        name = host
-    return host, name
-
-"""
-def getContentLength(data):
-    try:
-        len = int(data.split('Content-Length: ')[1].split(' ')[0])
-        return len
-    except:
-        return 0
-
-
-def recvn(sock, length):
-    buf = ''
-    for _ in range(length):
-        t = sock.recv(1)
-        if len(t) != 1:
-            return buf
-        buf += t
-
-        if len(buf) == length:
-            return buf
-
-
-def recvuntil(sock, delim):
-    ret = ''
-    while True:
-        t = sock.recv(1)
-        ret += t
-        if ret.endswith(delim):
-            return ret
-"""
 
 def genCert(name):
     cmd = 'cd ' + basedir + ' & _make_site.bat ' + name
@@ -62,6 +22,65 @@ def clearCert():
     os.system(cmd)
 
 
+def getInfo(data):
+    header = data.split(' ')
+    host = header[1].split(':')[0]
+    if host[:4] == 'www.':
+        name = host[4:]
+    else:
+        name = host
+    return host, name
+
+
+def getContentLength(data):
+    try:
+        len = int(data.split('Content-Length: ')[1].split(' ')[0])
+        return len
+    except:
+        return 0
+
+
+def recvn(sock, length):
+    buf = ''
+    for _ in range(length):
+        t = sock.recv(1)
+        if len(t) != 1:
+            return buf
+        buf += t
+        if len(buf) == length:
+            return buf
+
+
+def recvuntil(sock, delim):
+    ret = ''
+    while 1:
+        t = sock.recv(1)
+        ret += t
+        if ret.endswith(delim):
+            return ret
+
+
+def relaying1(sock):
+    res = ''
+    while 1:
+        buf = sock.recv(4096)
+        res += buf
+        if not buf:
+            break
+    return res
+
+def relaying2(sock):
+    res_header = recvuntil(sock, '\r\n\r\n')
+    content_length = getContentLength(res_header)
+    if content_length:
+        data = recvn(sock, content_length)
+    else:
+        data = ''
+
+    res = res_header + data
+    return res
+
+
 class SockHandler(BaseRequestHandler):
     def handle(self):
         self.data = self.request.recv(4096)
@@ -69,29 +88,31 @@ class SockHandler(BaseRequestHandler):
         if 'CONNECT' in self.data:
             host, name = getInfo(self.data)
             print host, name
-            self.request.sendall('HTTP/1.1 Connection established\r\n\r\n') #respond to client with 'connection established' message
+            self.request.sendall(
+                'HTTP/1.1 Connection established\r\n\r\n')  # respond to client with 'connection established' message
 
             genCert(name)
             client_sock = ssl.wrap_socket(self.request, server_side=True,
                                           keyfile=os.path.join(basedir, name + '.pem'),
                                           certfile=os.path.join(basedir, name + '.pem'))
 
-            req = client_sock.recv(4096)        #get request
+            req = client_sock.recv(4096)  # get request
 
-            try:    #openssl to real server
+            try:  # openssl to real server
                 server_sock = ssl.wrap_socket(socket.socket(socket.AF_INET, socket.SOCK_STREAM))
                 server_sock.connect((host, 443))
 
                 server_sock.sendall(req)
 
-                #relaying
-                while 1:
+                """
+                while 1:    
                     res = server_sock.recv(4096)
                     if not res:
                         break
-
+                """
+                res = relaying1(server_sock)
+                #res = relaying2(server_sock)
                 client_sock.sendall(res)
-                return
 
             except:
                 print "[+]Trying Connection...\n\n"
